0f57cbc98888fa91fd3f65c984a144958ed3f410
Andrew Hejl <andrew.hejl@outlook.com>
2021-03-27 18:35:09 -0700

    Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,20 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MiriamsStoreSln.Models;
+using MiriamsStoreSln.Models.ViewModels;
 
 namespace MiriamsStoreSln.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index()
+        public ViewResult Index(int productPage = 1)
+        => View(new ProductListViewModel
         {
-            return View(repository.Products);
-        }
+            Products = repository.Products
+        .OrderBy(p => p.ProductID)
+        .Skip((productPage - 1) * PageSize)
+        .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        });
     }
 }
---
+++ Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MiriamsStoreSln.Models;
+
+namespace MiriamsStoreSln.Models.ViewModels
+{
+
+        public class ProductListViewModel
+        {
+            public IEnumerable<Product> Products { get; set; }
+            public PagingInfo PagingInfo { get; set; }
+        }
+}
