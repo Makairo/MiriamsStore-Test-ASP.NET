1c5dbdeae26f9e7b47b790a8f912f255da956e76
Andrew Hejl <andrew.hejl@outlook.com>
2021-04-17 15:03:13 -0700

    Added admin pages and support

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace MiriamsStoreSln.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a product name")]
         public string ProductName { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         public string ProductDesc { get; set; }
-        [Column(TypeName = "decimal(8, 2)")]
+        [Required]
+        [Range(0.01, double.MaxValue,
+        ErrorMessage = "Please enter a positive price")]
         public decimal ProductPrice { get; set; }
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category {get; set;}
 
     }
